39b5e50ec36fee97c4007d823ded1aad9e04ffdc
Jon Cram <webignition@gmail.com>
2020-06-24 12:18:08 +0100

    Import ExternalVariableIdentifiersFactory (#11)

---
+++ src/Services/ExternalVariableIdentifiersFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCliCompiler\Services;
+
+use webignition\BasilCompiler\ExternalVariableIdentifiers;
+
+class ExternalVariableIdentifiersFactory
+{
+    public static function create(): ExternalVariableIdentifiers
+    {
+        return new ExternalVariableIdentifiers(
+            '$this->navigator',
+            '$_ENV',
+            'self::$client',
+            'self::$crawler',
+            '$this',
+            'self::$inspector',
+            'self::$mutator',
+            '$this->actionFactory',
+            '$this->assertionFactory'
+        );
+    }
+}
---
+++ tests/Unit/Services/ExternalVariableIdentifiersFactoryTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCliCompiler\Tests\Unit\Services;
+
+use webignition\BasilCliCompiler\Services\ExternalVariableIdentifiersFactory;
+use webignition\BasilCliCompiler\Tests\Unit\AbstractBaseTest;
+use webignition\BasilCompiler\ExternalVariableIdentifiers;
+
+class ExternalVariableIdentifiersFactoryTest extends AbstractBaseTest
+{
+    public function testCreate()
+    {
+        $this->assertEquals(
+            new ExternalVariableIdentifiers(
+                '$this->navigator',
+                '$_ENV',
+                'self::$client',
+                'self::$crawler',
+                '$this',
+                'self::$inspector',
+                'self::$mutator',
+                '$this->actionFactory',
+                '$this->assertionFactory'
+            ),
+            ExternalVariableIdentifiersFactory::create()
+        );
+    }
+}
