3d081f628b89f417fe678c9db80b679fb19290a7
Jon Cram <webignition@gmail.com>
2020-06-24 14:49:31 +0100

    Import ConfigurationFactory

---
+++ src/Services/ConfigurationFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCliCompiler\Services;
+
+use webignition\BasilCliCompiler\Model\Configuration;
+
+class ConfigurationFactory
+{
+    private string $projectRootPath;
+
+    public function __construct(string $projectRootPath)
+    {
+        $this->projectRootPath = $projectRootPath;
+    }
+
+    public function create(string $rawSource, string $rawTarget, string $baseClass): Configuration
+    {
+        return new Configuration(
+            (string) $this->getAbsolutePath($rawSource),
+            (string) $this->getAbsolutePath($rawTarget),
+            $baseClass
+        );
+    }
+
+    private function getAbsolutePath(string $path): ?string
+    {
+        if ('' === $path) {
+            return null;
+        }
+
+        $isAbsolutePath = '/' === $path[0];
+        if ($isAbsolutePath) {
+            return $this->getRealPath($path);
+        }
+
+        return $this->getRealPath($this->projectRootPath . '/' . $path);
+    }
+
+    private function getRealPath(string $path): ?string
+    {
+        $path = realpath($path);
+
+        return false === $path ? null : $path;
+    }
+}
---
+++ tests/Unit/Services/ConfigurationFactoryTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCliCompiler\Tests\Unit\Services;
+
+use webignition\BasilCliCompiler\Model\Configuration;
+use webignition\BasilCliCompiler\Services\ConfigurationFactory;
+use webignition\BasilCliCompiler\Services\ProjectRootPathProvider;
+use webignition\BasilCliCompiler\Tests\Unit\AbstractBaseTest;
+
+class ConfigurationFactoryTest extends AbstractBaseTest
+{
+    private ConfigurationFactory $factory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->factory = new ConfigurationFactory(
+            (new ProjectRootPathProvider())->get()
+        );
+    }
+
+    /**
+     * @dataProvider createFromTypedInputSuccessDataProvider
+     */
+    public function testCreate(
+        string $rawSource,
+        string $rawTarget,
+        string $baseClass,
+        Configuration $expectedConfiguration
+    ) {
+        $this->assertEquals($expectedConfiguration, $this->factory->create($rawSource, $rawTarget, $baseClass));
+    }
+
+    public function createFromTypedInputSuccessDataProvider(): array
+    {
+        $root = (new ProjectRootPathProvider())->get();
+
+        return [
+            'source and target resolve to absolute paths' => [
+                'rawSource' => 'tests/Fixtures/basil/Test/example.com.verify-open-literal.yml',
+                'rawTarget' => 'tests/build/target',
+                'baseClass' => AbstractBaseTest::class,
+                'expectedConfiguration' => new Configuration(
+                    $root . '/tests/Fixtures/basil/Test/example.com.verify-open-literal.yml',
+                    $root . '/tests/build/target',
+                    AbstractBaseTest::class
+                ),
+            ],
+        ];
+    }
+}
