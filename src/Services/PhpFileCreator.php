41f00882b78eb7b5d87b895c34c311c34871b5aa
Jon Cram <webignition@gmail.com>
2020-06-24 15:26:11 +0100

    Import PhpFileCreator (#16)

---
+++ src/Services/PhpFileCreator.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCliCompiler\Services;
+
+class PhpFileCreator
+{
+    private const TEMPLATE = <<< 'EOT'
+<?php
+
+namespace webignition\BasilCliCompiler\Generated;
+
+%s
+
+EOT;
+
+
+    private string $outputDirectory = '';
+
+    public function setOutputDirectory(string $outputDirectory): void
+    {
+        $this->outputDirectory = $outputDirectory;
+    }
+
+    public function create(string $className, string $code): string
+    {
+        $content = sprintf(self::TEMPLATE, $code);
+
+        $filename = $className . '.php';
+        $path = $this->outputDirectory . '/' . $filename;
+
+        file_put_contents($path, $content);
+
+        return $filename;
+    }
+}
