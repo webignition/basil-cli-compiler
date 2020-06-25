4aaa743b5f0a1e4913fc24202f3fe16c472ef885
Jon Cram <webignition@gmail.com>
2020-06-25 15:09:01 +0100

    Make ApplicationFactory::create() static (#41)

--- src/Services/ApplicationFactory.php
+++ src/Services/ApplicationFactory.php
@@ -7,34 +7,22 @@ namespace webignition\BasilCliCompiler\Services;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\SingleCommandApplication;
-use webignition\BasilCliCompiler\Command\GenerateCommand;
 
 class ApplicationFactory
 {
-    private GenerateCommand $command;
-
-    public function __construct(CommandFactory $commandFactory)
+    public static function create(): SingleCommandApplication
     {
-        $this->command = $commandFactory->createGenerateCommand();
-    }
+        $commandFactory = CommandFactory::createFactory();
+        $command = $commandFactory->createGenerateCommand();
 
-    public static function createFactory(): self
-    {
-        return new ApplicationFactory(
-            CommandFactory::createFactory()
-        );
-    }
-
-    public function create(): SingleCommandApplication
-    {
         $application = new SingleCommandApplication();
-        $application->setName((string) $this->command->getName());
-        $application->setDefinition($this->command->getDefinition());
+        $application->setName((string) $command->getName());
+        $application->setDefinition($command->getDefinition());
 
         $application
             ->setVersion('0.1-beta')
-            ->setCode(function (InputInterface $input, OutputInterface $output) {
-                return $this->command->run($input, $output);
+            ->setCode(function (InputInterface $input, OutputInterface $output) use ($command) {
+                return $command->run($input, $output);
             });
 
         return $application;
