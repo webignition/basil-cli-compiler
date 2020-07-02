d6d5df37b5f1f6c16ae90999d552744d65b8ed92
Jon Cram <webignition@gmail.com>
2020-07-02 09:16:29 +0100

    Use webignition/single-command-application-phar-builder

--- bin/compile.php
+++ bin/compile.php
@@ -2,13 +2,13 @@
 
 declare(strict_types=1);
 
+use webignition\SingleCommandApplicationPharBuilder\Builder;
+
 $root = (string) realpath(__DIR__ . '/..');
 
 require $root . '/vendor/autoload.php';
 
-use webignition\BasilCliCompiler\PharCompiler;
-
-$pharCompiler = new PharCompiler(
+$pharCompiler = new Builder(
     $root,
     'build/compiler.phar',
     'bin/compiler',
@@ -23,4 +23,4 @@ $pharCompiler = new PharCompiler(
     ]
 );
 
-$pharCompiler->compile();
+$pharCompiler->build();
