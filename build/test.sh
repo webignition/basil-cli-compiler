56016bc53e32670f64d873150a1522d795c9c714
Jon Cram <webignition@gmail.com>
2020-08-07 16:31:17 +0100

    Use tcp-cli-proxy-server 0.4 (#159)

--- build/test.sh
+++ build/test.sh
@@ -30,17 +30,28 @@ docker start ${CONTAINER_NAME}
 
 sleep 0.1
 
-( echo "./compiler --version"; echo "quit"; ) | nc localhost ${HOST_PORT}
-( echo "./compiler --source=${CONTAINER_SOURCE_PATH}/${CONTAINER_TEST_FILENAME} --target=${CONTAINER_TARGET_PATH}"; echo "quit"; ) | nc localhost ${HOST_PORT}
+( echo "./compiler --version"; ) | nc localhost ${HOST_PORT}
+printf "\n"
 
-EXPECTED_GENERATED_FILENAME="${HOST_TARGET_PATH}/Generated8a4077150b8e96cf57e90e6bf5dd6076Test.php"
+COMPILER_OUTPUT=$( ( echo "./compiler --source=${CONTAINER_SOURCE_PATH}/${CONTAINER_TEST_FILENAME} --target=${CONTAINER_TARGET_PATH}"; ) | nc localhost ${HOST_PORT} )
+printf "${COMPILER_OUTPUT}\n"
+
+if [[ $COMPILER_OUTPUT =~ (Generated.*\.php) ]]; then
+  GENERATED_TEST_FILENAME=${BASH_REMATCH}
+else
+  echo "x generated filename extraction failed"
+
+  return 1
+fi
+
+EXPECTED_GENERATED_FILENAME="${HOST_TARGET_PATH}/${GENERATED_TEST_FILENAME}"
 OUTPUT=$(ls ${EXPECTED_GENERATED_FILENAME} | wc -l)
 
 if [ ${OUTPUT} != "1" ]; then
-  echo "Test generation failed"
+  echo "x test generation failed"
   exit 1
 else
-  echo "Test generation successful"
+  echo "✓ test generation successful"
 fi
 
 rm -Rf ${HOST_DATA_PATH}
