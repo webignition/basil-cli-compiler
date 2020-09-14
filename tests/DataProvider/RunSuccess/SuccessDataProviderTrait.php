f60a114135ffb0e68e8df2090da4c472be666a87
Jon Cram <webignition@gmail.com>
2020-09-14 16:18:07 +0100

    Support multiple browsers per test source

--- tests/DataProvider/RunSuccess/SuccessDataProviderTrait.php
+++ tests/DataProvider/RunSuccess/SuccessDataProviderTrait.php
@@ -107,6 +107,16 @@ trait SuccessDataProviderTrait
                         ),
                         new TestManifest(
                             new TestModelConfiguration('chrome', 'https://example.com/'),
+                            $root . '/tests/Fixtures/basil/Test/example.com.verify-open-literal-multiple-browsers.yml',
+                            $root . '/tests/build/target/Generated4b5acb6ab789aa90fe19904bd0e5f458Test.php'
+                        ),
+                        new TestManifest(
+                            new TestModelConfiguration('firefox', 'https://example.com/'),
+                            $root . '/tests/Fixtures/basil/Test/example.com.verify-open-literal-multiple-browsers.yml',
+                            $root . '/tests/build/target/Generated3cac2ec205642b2a6a0cb7c4c2d04c45Test.php'
+                        ),
+                        new TestManifest(
+                            new TestModelConfiguration('chrome', 'https://example.com/'),
                             $root . '/tests/Fixtures/basil/Test/example.com.verify-open-literal.yml',
                             $root . '/tests/build/target/Generated4238ad333014be4c5d99e227b087cc9eTest.php'
                         ),
@@ -116,6 +126,8 @@ trait SuccessDataProviderTrait
                     'tests/Fixtures/php/Test/Generated462f42149b07d13071c2620afb561b30Test.php',
                     'tests/Fixtures/php/Test/Generated0c65b0d5e0e28f11ea3c8193ad9d162dTest.php',
                     'tests/Fixtures/php/Test/GeneratedAe219ca93cea5924090e0b5a2d2eea22Test.php',
+                    'tests/Fixtures/php/Test/Generated4b5acb6ab789aa90fe19904bd0e5f458Test.php',
+                    'tests/Fixtures/php/Test/Generated3cac2ec205642b2a6a0cb7c4c2d04c45Test.php',
                     'tests/Fixtures/php/Test/Generated4238ad333014be4c5d99e227b087cc9eTest.php',
                 ],
             ],
@@ -159,6 +171,37 @@ trait SuccessDataProviderTrait
                     'tests/Fixtures/php/Test/Generated0c65b0d5e0e28f11ea3c8193ad9d162dTest.php',
                     'tests/Fixtures/php/Test/Generated462f42149b07d13071c2620afb561b30Test.php',
                     'tests/Fixtures/php/Test/Generated4238ad333014be4c5d99e227b087cc9eTest.php',
+                ],
+            ],
+            'single test with multiple browsers' => [
+                'input' => [
+                    '--source' =>
+                        $root . '/tests/Fixtures/basil/Test/example.com.verify-open-literal-multiple-browsers.yml',
+                    '--target' => $root . '/tests/build/target',
+                ],
+                'expectedExitCode' => 0,
+                'expectedCommandOutput' => new SuiteManifest(
+                    new Configuration(
+                        $root . '/tests/Fixtures/basil/Test/example.com.verify-open-literal-multiple-browsers.yml',
+                        $root . '/tests/build/target',
+                        AbstractBaseTest::class
+                    ),
+                    [
+                        new TestManifest(
+                            new TestModelConfiguration('chrome', 'https://example.com/'),
+                            $root . '/tests/Fixtures/basil/Test/example.com.verify-open-literal-multiple-browsers.yml',
+                            $root . '/tests/build/target/Generated4b5acb6ab789aa90fe19904bd0e5f458Test.php'
+                        ),
+                        new TestManifest(
+                            new TestModelConfiguration('firefox', 'https://example.com/'),
+                            $root . '/tests/Fixtures/basil/Test/example.com.verify-open-literal-multiple-browsers.yml',
+                            $root . '/tests/build/target/Generated3cac2ec205642b2a6a0cb7c4c2d04c45Test.php'
+                        ),
+                    ]
+                ),
+                'expectedGeneratedCodePaths' => [
+                    'tests/Fixtures/php/Test/Generated4b5acb6ab789aa90fe19904bd0e5f458Test.php',
+                    'tests/Fixtures/php/Test/Generated3cac2ec205642b2a6a0cb7c4c2d04c45Test.php',
                 ],
             ],
         ];
