29820784c3ef7186a87d84a832b1c43317b5c767
Jon Cram <webignition@gmail.com>
2020-09-17 18:05:26 +0100

    Remove CompiledTest::test

--- src/Model/CompiledTest.php
+++ src/Model/CompiledTest.php
@@ -4,24 +4,15 @@ declare(strict_types=1);
 
 namespace webignition\BasilCliCompiler\Model;
 
-use webignition\BasilModels\Test\TestInterface;
-
 class CompiledTest
 {
-    private TestInterface $test;
     private string $code;
     private string $className;
 
-    public function __construct(TestInterface $test, string $code, string $className)
+    public function __construct(string $code, string $className)
     {
-        $this->test = $test;
         $this->code = $code;
         $this->className = $className;
-    }
-
-    public function getTest(): TestInterface
-    {
-        return $this->test;
     }
 
     public function getCode(): string
@@ -32,13 +23,5 @@ class CompiledTest
     public function getClassName(): string
     {
         return $this->className;
-    }
-
-    public function withTest(TestInterface $test): self
-    {
-        $new = clone $this;
-        $new->test = $test;
-
-        return $new;
     }
 }
--- tests/Functional/Services/TestWriterTest.php
+++ tests/Functional/Services/TestWriterTest.php
@@ -6,8 +6,6 @@ namespace webignition\BasilCliCompiler\Tests\Functional\Services;
 
 use webignition\BasilCliCompiler\Model\CompiledTest;
 use webignition\BasilCliCompiler\Services\TestWriter;
-use webignition\BasilModels\Test\ConfigurationInterface;
-use webignition\BasilModels\Test\TestInterface;
 
 class TestWriterTest extends \PHPUnit\Framework\TestCase
 {
@@ -44,21 +42,9 @@ class TestWriterTest extends \PHPUnit\Framework\TestCase
     {
         $root = getcwd();
 
-        $testConfiguration = \Mockery::mock(ConfigurationInterface::class);
-
-        $test = \Mockery::mock(TestInterface::class);
-        $test
-            ->shouldReceive('getPath')
-            ->andReturn('test.yml');
-
-        $test
-            ->shouldReceive('getConfiguration')
-            ->andReturn($testConfiguration);
-
         return [
             'default' => [
                 'compiledTest' => new CompiledTest(
-                    $test,
                     'compiled test code',
                     'ClassName'
                 ),
