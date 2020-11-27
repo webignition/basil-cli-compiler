7ff1723d92e65e87d468c97fee703db5dff2f024
Jon Cram <webignition@gmail.com>
2020-11-27 15:44:30 +0000

    Try things out 00005

--- build/poc2.sh
+++ build/poc2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#CURRENT_DIRECTORY="$(dirname "$0")"
-#source ${CURRENT_DIRECTORY}/poc.sh
+CURRENT_DIRECTORY="$(dirname "$0")"
+source ${CURRENT_DIRECTORY}/poc.sh
 
 echo "Image name in 2: "${IMAGE_NAME}
