d602e280122fd99465ccbe69646bca8b2b78d8d5
Jon Cram <webignition@gmail.com>
2020-11-30 10:42:22 +0000

    Switch to github actions (#216)

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-CURRENT_DIRECTORY="$(dirname "$0")"
-source ${CURRENT_DIRECTORY}/.image_data.sh
-
-docker build -f docker/Dockerfile -t ${IMAGE_NAME} .
+docker build -f docker/Dockerfile -t "smartassert/basil-compiler:${TAG_NAME:-master}" .
